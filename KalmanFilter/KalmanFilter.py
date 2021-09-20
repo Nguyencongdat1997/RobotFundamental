c7f8a048b34ba0cd89012276eb2922823a40b483
GattoMac <traceformula2000@gmail.com>
2021-09-19 21:49:44 -0700

    KalmanFilter | Introduction

---
+++ KalmanFilter/KalmanFilter.py
@@ -0,0 +1,12 @@
+class KalmanFilter:
+    def __init__(self, mu, sigma, ut, zt):
+        '''
+
+
+
+        :param mu: mean vector at time t-1. Shape (n,1). with n is the shape of  state x (shape(X)= (n,1))
+        :param sigma: covariance matrix at time t-1. Shape
+        :param ut: action at time t
+        :param zt: sensor data at time t
+        '''
+        pass
